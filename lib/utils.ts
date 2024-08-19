494d2869b4257ad5dff0ecbcac030be29355286b
Vru007 <vrushik7143work@gmail.com>
2024-08-19 19:01:33 +0530

    user_actions(CRUD)

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,4 +5,19 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-//cececewkjcbkjbcknwc
+export const handleError=(error:unknown)=>{
+  if(error instanceof Error){
+
+    console.error(error.message);
+    throw new Error(`Error: ${error.message}`);
+  }
+  else if(typeof error==="string"){
+    console.error(error);
+    throw new Error(`Error: ${error}`);
+  }
+  else{
+    console.error(error);
+    throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+  }
+};
+
